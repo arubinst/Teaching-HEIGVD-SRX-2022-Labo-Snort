8f6c85012cc6ac10db541cd1aa062350e0eba10f
Luhrel <57545107+Luhrel@users.noreply.github.com>
2022-04-08 10:51:54 +0200

    Add containers scripts

---
+++ scripts-containers/ids.sh
@@ -0,0 +1,5 @@
+nft add table nat
+nft 'add chain nat postrouting { type nat hook postrouting priority 100 ; }'
+nft add rule nat postrouting meta oifname "eth0" masquerade
+
+apt update && apt install snort -y
