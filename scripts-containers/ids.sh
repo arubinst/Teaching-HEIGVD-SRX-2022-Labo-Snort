a42526070eb27bd8ba9bc2873fdd8dde39c4798e
Luhrel <57545107+Luhrel@users.noreply.github.com>
2022-04-08 11:44:53 +0200

    Add /root/myrules.rules

--- scripts-containers/ids.sh
+++ scripts-containers/ids.sh
@@ -10,3 +10,4 @@ echo "include /etc/snort/rules/icmp2.rules" > /etc/snort/mysnort.conf
 
 echo 'alert icmp any any -> any any (msg:"ICMP Packet"; sid:4000001; rev:3;)' > /etc/snort/rules/icmp2.rules
 
+echo "alert tcp any any -> any any (msg:\"Concerne l'HEIG-VD\"; content:\"HEIG-VD\"; sid:1000001; rev:1;)" > /root/myrules.rules
