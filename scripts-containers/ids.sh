94db1371bc79700aa3c5f1928a159bf1d8eb8050
Luhrel <57545107+Luhrel@users.noreply.github.com>
2022-04-08 11:02:21 +0200

    Add basic custom config files

--- scripts-containers/ids.sh
+++ scripts-containers/ids.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
 nft add table nat
 nft 'add chain nat postrouting { type nat hook postrouting priority 100 ; }'
 nft add rule nat postrouting meta oifname "eth0" masquerade
 
 apt update && apt install snort -y
+
+echo "include /etc/snort/rules/icmp2.rules" > /etc/snort/mysnort.conf
+
+echo 'alert icmp any any -> any any (msg:"ICMP Packet"; sid:4000001; rev:3;)' > /etc/snort/rules/icmp2.rules
+
